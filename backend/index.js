15783375799931778c4648bafb86eaec5bca4830
Ahamed Bilal <ahamedbilalazmy@gmail.com>
2022-07-10 15:26:18 +0530

    complete

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,17 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 
-const db = mysql.createPool({
-    host: 'mysql_db', // the host name MYSQL_DATABASE: node_mysql
-    user: 'MYSQL_USER', // database user MYSQL_USER: MYSQL_USER
-    password: 'MYSQL_PASSWORD', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
-    database: 'books' // database name MYSQL_HOST_IP: mysql_db
-});
-
-app.use(cors())
-
 const app = express();
+app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'books'
+});
 
 app.get('/', (req, res) => {
     res.send('Hi There')
@@ -33,18 +31,19 @@ app.post("/insert", (req, res) => {
     const bookReview = req.body.setReview;
     const InsertQuery = "INSERT INTO books_reviews (book_name, book_review) VALUES (?, ?)";
     db.query(InsertQuery, [bookName, bookReview], (err, result) => {
-      console.log(result)
+        console.log(result)
     })
-  })
+})
 
-  app.put("/update/:bookId", (req, res) => {
+app.put("/update/:bookId", (req, res) => {
     const bookReview = req.body.reviewUpdate;
     const bookId = req.params.bookId;
     const UpdateQuery = "UPDATE books_reviews SET book_review = ? WHERE id = ?";
     db.query(UpdateQuery, [bookReview, bookId], (err, result) => {
-      if (err) console.log(err)
+        if (err) console.log(err)
     })
-  })
+})
 
-  app.listen('3001', () => { })
+app.listen('3001', () => {
+})
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Button, Container, Card, Row } from 'react-bootstrap';
+
+axios.defaults.baseURL = 'https://localhost:3001/';
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    bookName: '',
+    review: ''
+  });
+  const [fetchData, setFetchData] = useState([]);
+  const [reviewUpdate, setReviewUpdate] = useState('');
+
+
+  useEffect(() => {
+    axios.get("/api/get")
+      .then((response) => {
+        setFetchData(response.data);
+      });
+  }, [])
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleChange2 = (event) => {
+    setReviewUpdate(event.target.value);
+  }
+
+
+  const submit = () => {
+    axios.post('/api/insert', this.state)
+      .then(() => { alert('success post') })
+    console.log(this.state)
+    document.location.reload();
+  };
+
+  const deleteData = (id) => {
+    if (window.confirm("Do you want to delete? ")) {
+      axios.delete(`/api/delete/${id}`)
+      document.location.reload()
+    }
+  };
+
+  const editData = (id) => {
+    axios.put(`/api/update/${id}`, this.state)
+    document.location.reload();
+  };
+
+  let card = fetchData.map((val, key) => {
+    return (
+      <React.Fragment>
+        <Card style={{ width: '18rem' }} className='m-2'>
+          <Card.Body>
+            <Card.Title>{val.book_name}</Card.Title>
+            <Card.Text>
+              {val.book_review}
+            </Card.Text>
+            <input name='reviewUpdate' onChange={handleChange2} placeholder='Update Review' ></input>
+            <Button className='m-2' onClick={() => { editData(val.id) }}>Update</Button>
+            <Button onClick={() => { deleteData(val.id) }}>Delete</Button>
+          </Card.Body>
+        </Card>
+      </React.Fragment>
+    )
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Dockerized Fullstack React Application</h1>
+      <div className='form'>
+        <input name='bookName' value={formData.bookName} placeholder='Enter Book Name' onChange={handleChange} />
+        <input name='review' value={formData.review} placeholder='Enter Review' onChange={handleChange} />
+      </div>
+      <Button className='my-2' variant="primary" onClick={submit}>Submit</Button> <br /><br />
+      <Container>
+        <Row>
+          {card}
+        </Row>
+      </Container>
     </div>
   );
 }
